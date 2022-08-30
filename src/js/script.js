4072704d136955965afe1bfd1f1d17a38eb1e523
HolohotsIhor <gologocigor@gmail.com>
2022-08-30 21:21:40 +0300

    Created header

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,30 @@
 @@include('../../node_modules/jquery/dist/jquery.min.js');
 @@include('../../node_modules/bootstrap/dist/js/bootstrap.bundle.js');
+@@include('../../node_modules/owl.carousel/dist/owl.carousel.min.js');
 @@include('../libs/burger/burger.js');
 @@include('../libs/scroll-to-top/scroll-to-top.js');
 
 $(document).ready(function() {
-  console.log("you!");
-}); 
+    /* Owl carousel */
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        nav: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 100000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            960: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
+});
--- src/libs/burger/burger.js
+++ src/libs/burger/burger.js
@@ -1,30 +1,16 @@
 $(document).ready(function() {
     const toggleMenu = $(".toggle-menu");
     const sandwich = $(".toggle-menu__sandwich");
-    const topMenu =  $(".top_mnu");
-    const topText = $(".top_text");
+    const topMenu =  $(".top-menu");
+    const menuParent = $(".menu-item-has-children");
 
     /* Burger */
     toggleMenu.click(function() {
         sandwich.toggleClass("active");
-    });
-    /* Burger. Hover */
-    toggleMenu.click(function() {
-        topMenu.fadeOut(300);
-        sandwich.toggleClass("active");
-        topText.css("opacity", "1");
+        topMenu.toggleClass("active");
     });
 
-    /* Burger. Open and close */
-    toggleMenu.click(function() {
-        if (topMenu.is(":visible")) {
-            topText.css("opacity", "1");
-            topMenu.fadeOut(300);
-            sandwich.removeClass("active");
-        } else {
-            topText.css("opacity", ".1");
-            topMenu.fadeIn(300);
-            sandwich.addClass("active");
-        }
+    menuParent.click(function() {
+        $(this).toggleClass("active");
     });
 });
